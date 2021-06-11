2b813ef542e2aa388948e3ebf817a6f73a2e9b23
dillon <dillon@163.com>
2021-06-11 13:09:16 +0800

    提交demo

---
+++ src/test/java/com/basic/BasicAlgorithmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicAlgorithmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
