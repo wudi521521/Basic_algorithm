bdd0166ecf1a63404ef8199340570106df88c0b9
dillon <dillon@163.com>
2021-08-19 09:53:25 +0800

    Thread线程同步代码块,同步方法

---
+++ src/main/java/com/thread/basic/simple/chapter5/SynchronizeTest.java
@@ -0,0 +1,29 @@
+package com.thread.basic.simple.chapter5;
+
+/**
+ * @author Dillon Wu
+ * @Description:
+ * @date 2021/8/18 21:22
+ */
+public class SynchronizeTest {
+    private final static Object MONITOR = new Object();
+
+    public static void main(String[] args) {
+        Runnable runnable = ()->{
+            synchronized (MONITOR){
+                try {
+                    Thread.sleep(100_000);
+                }catch (InterruptedException e){
+
+                }
+            }
+        };
+        Thread t1 = new Thread(runnable);
+        Thread t2 = new Thread(runnable);
+        Thread t3 = new Thread(runnable);
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+
+}
