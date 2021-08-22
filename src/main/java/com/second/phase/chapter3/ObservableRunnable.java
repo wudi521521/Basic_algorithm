f6d1fa92ee80d48c6de72726b234ab9fe65e9c7d
dillon <dillon@163.com>
2021-08-22 21:15:05 +0800

    观察者设计模式,以及线程中观察者生命周期

---
+++ src/main/java/com/second/phase/chapter3/ObservableRunnable.java
@@ -0,0 +1,48 @@
+package com.second.phase.chapter3;
+
+/**
+ * @author Dillon Wu
+ * @Description: 观察者
+ * @date 2021/8/22 20:43
+ */
+public abstract class ObservableRunnable implements Runnable {
+
+    final protected LifeCycleListener listener;
+
+    public ObservableRunnable(final LifeCycleListener listener){
+        this.listener = listener;
+    }
+
+    protected void notifyChange(final RunnableEvent event){
+        listener.onEvent(event);
+    }
+
+    public enum RunnableState{
+        RUNNING,ERROR,DONE
+    }
+
+    public static class RunnableEvent{
+        private final RunnableState state;
+        private final Thread thread;
+        private final Throwable cause;
+
+        public RunnableEvent(RunnableState state,Thread thread,Throwable cause){
+            this.state=state;
+            this.thread = thread;
+            this.cause = cause;
+        }
+
+        public RunnableState getState() {
+            return state;
+        }
+
+        public Thread getThread() {
+            return thread;
+        }
+
+        public Throwable getCause() {
+            return cause;
+        }
+    }
+
+}
